9d63e4f235a546bd810527e790f2e9fcefbe2e5c
binogabriel <gabrielfv.almeida@gmail.com>
2022-06-02 18:19:43 -0300

    Projeto_v2

--- app/src/main/java/com/example/projeto_motivation/UserActivity.kt
+++ app/src/main/java/com/example/projeto_motivation/UserActivity.kt
@@ -1,13 +1,15 @@
 package com.example.projeto_motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.example.projeto_motivation.databinding.ActivityMainBinding
 import com.example.projeto_motivation.databinding.ActivityUserBinding
 
 
-class UserActivity : AppCompatActivity(), View.OnClickListener{
+class UserActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityUserBinding
 
@@ -23,8 +25,15 @@ class UserActivity : AppCompatActivity(), View.OnClickListener{
     }
 
     override fun onClick(v: View) {
-        if (v.id == R.id.button_save){
-
+        if (v.id == R.id.button_save) {
+            handleSave()
         }
+    }
+
+    private fun handleSave() {
+        if (binding.editName.text.toString() != "") {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        } else Toast.makeText(this, R.string.mandatory_name, Toast.LENGTH_SHORT).show()
     }
 }
